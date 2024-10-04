f03415ba1681d3ee16412925ec150f3a039e6b95
Subash <subashsk11831@gmail.com>
2024-10-04 19:02:57 +0530

    Subash

--- api/server.js
+++ api/server.js
@@ -1,7 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
 
-app.listen(8800, ()=>{
-    console.log("Backend server is Running!")
-})
+
+const app = express();
+
+mongoose.set('strictQuery', true);
+
+async function connect() {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connect();
+
+app.listen(8800, () => {
+  console.log("Backend server is Running!");
+});
