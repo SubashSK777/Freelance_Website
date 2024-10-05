49f876816e4ba17ac58e3bf6fc7b6ec41beaaef5
Subash <subashsk11831@gmail.com>
2024-10-05 19:44:47 +0530

    Ahhhhhhhhh Fuckkkk

--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js"
+import gigRoute from "./routes/gig.route.js"
+import orderRoute from "./routes/order.route.js"
+import conversationRoute from "./routes/conversation.route.js"
+import messageRoute from "./routes/message.route.js"
+import reviewRoute from "./routes/review.route.js"
+
 
 dotenv.config();
 
@@ -16,7 +23,15 @@ async function connect() {
   } catch (error) {
     console.log(error);
   }
-}
+};
+
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
+
 
 connect();
 
